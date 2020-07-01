96f98e936bf2a2f020b6ca87ca0018676facf98c
Vincent Yang <vincentyangcsi@gmail.com>
2020-07-01 20:02:48 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,7 +3,7 @@ def find_even_values(src)
   while row_index < src.count do
     element_index = 0
     while element_index < src[row_index].count do
-      if src[row_index][element_index].even?
+      if src[row_index][element_index] % 2 == 0
         p src[row_index][element_index]
       end
       element_index += 1
